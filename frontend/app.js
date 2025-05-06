c1e019d72f958fa6567fa3baac1a639e9f274f2a
sameekshya1999 <bcsamikshya@gmail.com>
2025-05-06 17:38:37 +0000

    Initial commit of Matrix-App

--- frontend/app.js
+++ frontend/app.js
@@ -4,18 +4,22 @@ angular.module('MatrixApp', [])
     $scope.useOpenMP = false;
     $scope.result = null;
     $scope.error = null;
+    $scope.loading = false;
 
     $scope.submit = function() {
         $scope.result = null;
         $scope.error = null;
+        $scope.loading = true;
 
         $http.post('/calculate', {
             size: $scope.size,
             use_openmp: $scope.useOpenMP
         }).then(function(response) {
             $scope.result = response.data;
+            $scope.loading = false;
         }, function(error) {
             $scope.error = error.data.error || 'An error occurred';
+            $scope.loading = false;
         });
     };
 }]);
